1a1d98daee81b211b798bcee1121f051dcfbadba
Jean-François VIAL <jeff@modulaweb.fr>
2018-05-28 16:03:05 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+postconf -e mydomain=$DOMAIN
+postconf -e myorigin=$DOMAIN
+postconf -e myhostname=$HOSTNAME
+postconf -e mynetworks="127.0.0.0/8 192.168.0.0/16 10.0.0.0/8 172.16.0.0/12"
+/usr/lib/postfix/post-install meta_directory=/etc/postfix create-missing
+/usr/lib/postfix/master &
+exec /usr/sbin/rsyslogd -n
