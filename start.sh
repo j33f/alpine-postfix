38aaadd90d077ed2bb9903ba3253820de8f53b9f
Jean-François VIAL <jeff@modulaweb.fr>
2018-05-28 16:39:08 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,5 @@ postconf -e myhostname=$HOSTNAME
 postconf -e mynetworks="127.0.0.0/8 192.168.0.0/16 10.0.0.0/8 172.16.0.0/12"
 /usr/lib/postfix/post-install meta_directory=/etc/postfix create-missing
 /usr/lib/postfix/master &
+rm /var/run/rsyslogd.pid
 exec /usr/sbin/rsyslogd -n
